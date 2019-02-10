12224e772d5ef89e40e33bb1fe8adf367cce5f7c
Jie He <Jiehe1998yd@gmail.com>
2019-02-10 22:08:42 +0000

    + Fixed result display formatting   + Added displayInfo func   + Added Star button & Change rating method

--- src/ResultPanel.java
+++ src/ResultPanel.java
@@ -9,12 +9,12 @@ public class ResultPanel extends JPanel {
 
     /** Demo samples **/
     private Sample samples[] = {
-            new Sample("SNARE(10).wav", 3, new String[]{"Snare", "Clap", "etc"}, "Jack", new Date(), "Just a demo", "Nowhere"),
-            new Sample("SMD_Snare_10.wav", 4, new String[]{"Sexy", "Drum"}, "Ross",new Date(), "Just a demo", "Nowhere"),
-            new Sample("Kick Puncher", 5, new String[]{"Kick", "Something", "Nice"}, "Jie", new Date(), "Just a demo", "Nowhere"),
-            new Sample("Kick Agile Shot", 2, new String[]{"Kick", "Something", "Nice"}, "Ross",new Date(), "Just a demo", "Nowhere"),
-            new Sample("Kick 1.wav", 5, new String[]{"Deep", "Something", "Nice"}, "Jie", new Date(), "Just a demo", "Nowhere"),
-            new Sample("Hihat Quick Watcher", 4, new String[]{"Something", "Nice"}, "Ross",new Date(), "Just a demo", "Nowhere"),
+            new Sample("SNARE(10).wav",     3, new String[]{"Snare", "Clap", "etc"}, "Jack", new Date(), "Just a demo", "Nowhere"),
+            new Sample("SMD_Snare_10.wav",  4, new String[]{"Sexy", "Drum"}, "Ross",new Date(), "Just a demo", "Nowhere"),
+            new Sample("Kick Puncher",      5, new String[]{"Kick", "Something", "Nice"}, "Jie", new Date(), "Just a demo", "Nowhere"),
+            new Sample("Kick Agile Shot",   2, new String[]{"Kick", "Something", "Nice"}, "Ross",new Date(), "Just a demo", "Nowhere"),
+            new Sample("Kick 1.wav",        5, new String[]{"Deep", "Something", "Nice"}, "Jie", new Date(), "Just a demo", "Nowhere"),
+            new Sample("Hihat Quick Watcher",4, new String[]{"Something", "Nice"}, "Ross",new Date(), "Just a demo", "Nowhere"),
             new Sample("Hihat Closed Corner", 2, new String[]{"Something", "Nice"}, "Jie", new Date(), "Just a demo", "Nowhere"),
             new Sample("bcg kick.wav", 2, new String[]{"Something", "Nice"}, "Ross",new Date(), "Just a demo", "Nowhere"),
             new Sample("A-Conga Low Slap 2", 4, new String[]{"Lowkey", "Slap", "Something", "Nice"}, "Jie", new Date(), "Just a demo", "Nowhere"),
@@ -23,20 +23,26 @@ public class ResultPanel extends JPanel {
 
     private SampleListItem sli;
     private JPanel resultView;
-    private JScrollPane listview;
+    private JScrollPane scrollResultView;
 
     public ResultPanel(SampleSafe ss){
         this.ss = ss;
+        setLayout(new BorderLayout());
 
-        setBackground(Color.red);
-
+        // This is the panel containing the list items
         resultView = new JPanel();
         resultView.setLayout(new BoxLayout(resultView, BoxLayout.Y_AXIS));
-        resultView.setBackground(Color.GRAY);
 
-        listview = new JScrollPane(resultView);
-        listview.setPreferredSize(new Dimension(250, 500));
-        add(listview);
+        // Make scroll pane from result view
+        scrollResultView = new JScrollPane(resultView,  JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                                                        JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollResultView.getVerticalScrollBar().setUnitIncrement(0x0A);
+        scrollResultView.setBorder(new EmptyBorder( 0x19,0x19,0x19,0x19));
+
+        // Add scroll pane
+        add(scrollResultView, BorderLayout.CENTER);
+
+        // demostration of displaying samples
         displayResult(samples);
    }
 
@@ -51,12 +57,18 @@ public class ResultPanel extends JPanel {
        // Instantiate new sample list item components
        for(int i = 0; i < result.length; i++){
            // Pass display sample
-           sli = new SampleListItem(result[i]);
-           sli.setPreferredSize(new Dimension(220, 100));
-           sli.setBorder(new EmptyBorder(10, 10, 0, 10));
+           sli = new SampleListItem(result[i], this);
+           //sli.setPreferredSize(new Dimension(220, 100));
+           sli.setBorder(new EmptyBorder(10, 10, 10, 10));
            // Add to view
            resultView.add(sli);
        }
+   }
+
+   protected void displayInfo(Sample sample){
+       // ss.displayInfo(sample);
+       JOptionPane.showMessageDialog(null, "Trying to display: " + sample.getTitle());
+       // waiting for rely method in SampleSafe.java
    }
 
 }
--- src/SampleListItem.java
+++ src/SampleListItem.java
@@ -4,67 +4,132 @@
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SampleListItem extends JPanel {
+    private final int sCount = 5;
+    private ResultPanel resultPanel;
 
     private Sample sample;
     private JLabel title;
-    private JLabel stars;
 
-    private JPanel topPanel;
-    private JPanel belPanel;
+    private JPanel  topPanel;
+    private JPanel  btnPanel;
+    private JButton btnStars[];
 
-    public SampleListItem(Sample sample){
-        this.sample = sample;
+    public SampleListItem(Sample sample, ResultPanel resultPanel){
+        this.sample      = sample;
+        this.resultPanel = resultPanel;
         setLayout(new BorderLayout());
-        setBackground(Color.WHITE);
+        LineBorder line = new LineBorder(Color.blue, 2, true);
+        setBorder(line);
 
-        /** Assigning sample name to the text of title label **/
+        /* Add the two labels to topPanel */
+        topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        topPanel.setBorder(new EmptyBorder(0x00, 0x0A, 0x00, 0x00));
+        //topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
+
+        /* Assigning sample name to the text of title label */
         title = new JLabel(sample.getTitle());
         title.setFont(new Font("Arial", Font.BOLD, 14));
-        title.setBorder(new EmptyBorder(10, 20, 0, 0)); // T, L, B, R
+        title.setBorder(new EmptyBorder(0x00, 0x0F, 0x00, 0x00)); // T, L, B, R
 
-        /** Assigning number of stars to the text of stars label **/
-        StringBuilder ratingString = new StringBuilder("☆ ☆ ☆ ☆ ☆");
-        for(int i = 0; i < sample.getStars() * 2 ; i+=2){
-            ratingString.setCharAt(i, '★');
+        /* Assigning number of stars to the text of stars label */
+        btnStars = new JButton[sCount];
+        for(int i = 0; i < sCount ; i++) {
+            btnStars[i] = new JButton();
+            btnStars[i].setPreferredSize(new Dimension(36,36));
+            btnStars[i].setBorder(BorderFactory.createEmptyBorder());
+            btnStars[i].setFont(new Font("Serif", Font.PLAIN, 24));
+            btnStars[i].setName("" + i);
+            btnStars[i].addActionListener(new starButtonListener(i));
+            topPanel.add(btnStars[i]);
         }
-        stars = new JLabel(ratingString.toString());
-        stars.setFont(new Font("serif", Font.BOLD, 24));
-        stars.setBorder(new EmptyBorder(0, 20, 0, 0)); // T, L, B, R
-
-        /** Add the two labels to topPanel **/
-        topPanel = new JPanel();
-        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
-        topPanel.add(title);
-        topPanel.add(stars);
+        highlightStars(sample.getStars());
 
         /** Create tags & add to belPanel **/
-        belPanel = new JPanel();
+        btnPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        btnPanel.setBorder(new EmptyBorder(0x00, 0x0A, 0x00, 0x00));
+
         String[] tags = sample.getTags();
-        for(int i = 0; i < tags.length; i++){
-            belPanel.add(new JButton(tags[i]));
+        for (String tag : tags) {
+            btnPanel.add(new JButton(tag));
         }
 
-        /** Add panels to this **/
-        add(topPanel, BorderLayout.PAGE_START);
-        add(belPanel);
+        /* Add panels to this **/
+        add(title, BorderLayout.PAGE_START);
+        add(topPanel, BorderLayout.LINE_START);
+        add(btnPanel, BorderLayout.PAGE_END);
 
-        /** Hover & Exit Colors **/
+        /* Hover & Exit Colors **/
         Color defColor = UIManager.getColor("Panel.background");
         this.addMouseListener(new java.awt.event.MouseAdapter(){
             public void mouseEntered(java.awt.event.MouseEvent evt){
-                setBackground(Color.GRAY);
-                topPanel.setBackground(Color.GRAY);
-                belPanel.setBackground(Color.GRAY);
+                setBackground(Color.lightGray);
+                topPanel.setBackground(Color.lightGray);
+                btnPanel.setBackground(Color.lightGray);
             }
 
             public void mouseExited(java.awt.event.MouseEvent evt){
-                setBackground(Color.WHITE);
+                setBackground(defColor);
                 topPanel.setBackground(defColor);
-                belPanel.setBackground(defColor);
+                btnPanel.setBackground(defColor);
+            }
+
+            public void mousePressed(java.awt.event.MouseEvent evt){
+                resultPanel.displayInfo(sample);
             }
         });
     }
+
+    /**
+     * Method for changing the rating of a displayed sample
+     * @param rating new rating
+     */
+    private void changeRating(int rating){
+        if(rating != sample.getStars()){
+            highlightStars(rating);
+            sample.setStars(rating);
+        }
+
+    }
+
+    /**
+     * Updates each star component
+     * @param rating new rating
+     */
+    private void highlightStars(int rating){
+        JButton b;
+        for(int i = 0; i < sCount; i++){
+            b = btnStars[i];
+            if( i <= rating ){
+                b.setText("★");
+                //b.setForeground(Color.YELLOW);
+            }else{
+                b.setText("☆");
+                b.setForeground(Color.BLACK);
+            }
+        }
+
+    }
+
+    /**
+     * Button listener for star buttons
+     */
+    public class starButtonListener implements ActionListener{
+        int c ;
+        public starButtonListener(int c){
+            super();
+            this.c = c;
+        }
+
+       public void actionPerformed(ActionEvent e){
+           changeRating(c);
+       }
+    }
 }
+
+
