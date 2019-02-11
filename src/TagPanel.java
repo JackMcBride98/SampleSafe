80a0e7cdb06e7e4817be928715fd28a10b6c6e07
Jie He <Jiehe1998yd@gmail.com>
2019-02-11 12:30:29 +0000

    + Added LoadTags function to TagPanel

--- src/TagPanel.java
+++ src/TagPanel.java
@@ -5,13 +5,23 @@ import java.awt.*;
 public class TagPanel extends JPanel {
     private SampleSafe  ss;
     private String      tags[];
+
+    public TagPanel(SampleSafe ss){
+        this.ss     = ss;
+        this.setLayout( new FlowLayout(FlowLayout.LEFT));
+        this.setBorder(new EmptyBorder(0x00, 0x0A, 0x00, 0x00));
+    }
+
     public TagPanel(SampleSafe ss, String[] tags){
         this.ss     = ss;
         this.tags   = tags;
 
         this.setLayout( new FlowLayout(FlowLayout.LEFT));
         this.setBorder(new EmptyBorder(0x00, 0x0A, 0x00, 0x00));
+    }
 
+    public void loadTags(String[] tags){
+        this.removeAll();
         for (String tag : tags) {
             this.add(new JButton(tag));
         }
