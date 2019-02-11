9a80eeceb50788eeb7f200cb852dc239b6370b12
Jie He <Jiehe1998yd@gmail.com>
2019-02-11 12:06:37 +0000

    + Added display info function to SS

--- src/ResultPanel.java
+++ src/ResultPanel.java
@@ -67,7 +67,7 @@ public class ResultPanel extends JPanel {
 
    protected void displayInfo(Sample sample){
        // ss.displayInfo(sample);
-       JOptionPane.showMessageDialog(null, "Trying to display: " + sample.getTitle());
+       //JOptionPane.showMessageDialog(null, "Trying to display: " + sample.getTitle());
        ss.displaySample(sample);
        // waiting for rely method in SampleSafe.java
    }
