c82341f021f21fb107696b84e6262e9d092acc3c
Jie He <Jiehe1998yd@gmail.com>
2019-02-20 16:06:12 +0000

    + Appened && Remove    + F' Ton's of bugs

--- src/TagPanel.java
+++ src/TagPanel.java
@@ -5,10 +5,12 @@ import java.awt.*;
 public class TagPanel extends JPanel {
     private SampleSafe  ss;
     private String      tags[];
+    private boolean     shouldDelete;
 
     public TagPanel(SampleSafe ss){
         this.ss     = ss;
         this.setLayout( new FlowLayout(FlowLayout.LEFT));
+        this.shouldDelete = true;
     }
 
     public TagPanel(SampleSafe ss, String[] tags){
@@ -23,10 +25,64 @@ public class TagPanel extends JPanel {
 
     public void loadTags(String[] tags){
         this.removeAll();
+        this.tags = tags;
         for (String tag : tags) {
-            this.add(new JButton(tag));
+            JButton btnTag = new JButton(tag);
+
+            btnTag.addMouseListener(new java.awt.event.MouseAdapter(){
+                public void mousePressed(java.awt.event.MouseEvent evt){
+                    deleteTag(btnTag.getText());
+                }
+            });
+
+            this.add(btnTag);
         }
         revalidate();
+    }
+
+    public void triggerDelete(boolean t){
+        shouldDelete = t;
+    }
+
+    private void deleteTag(String tag){
+        if(shouldDelete){
+
+            loadTags(remove(tags, tag));
+        }
+    }
+
+    public void addTag(String tag){
+        loadTags(appened(tags, tag));
+    }
+
+
+    public String[] get_tags(){
+        return tags;
+    }
+
+    private String[] appened(String[] t, String a){
+        String[] new_tags = new String[tags.length+1];
+        for (int i = 0; i < t.length; i++)
+        {
+            new_tags[i] = t[i];
+        }
+        new_tags[new_tags.length - 1] = a;
+        return new_tags;
+    }
+
+    private String[] remove(String[] t, String a){
+        if(t.length > 0){
+            String[] new_tags = new String[t.length-1];
+            int k = 0;
+            for(int i = 0; i < t.length; i++){
+                if(!t[i].equals(a)){
+                    if(i == t.length-1)
+                        return t;
+                    new_tags[k++] = t[i];
+                }
+            }
+        }
+        return t;
     }
 
 }
