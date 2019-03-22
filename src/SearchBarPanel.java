ddbfc42d518e258ac6f57f77d82d62dd890a6146
Jie He <Jiehe1998yd@gmail.com>
2019-03-22 20:00:55 +0000

    +Added SortOptionJFrame

--- src/SearchBarPanel.java
+++ src/SearchBarPanel.java
@@ -1,17 +1,52 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SearchBarPanel extends JPanel {
 
+    SortOptionJFrame sortPopup;
+    JFrame ss;
     public SearchBarPanel() {
         JButton searchButton = new JButton("SEARCH");
         JButton filterButton = new JButton("FILTER");
         JButton sortButton = new JButton("SORT");
         JTextField searchField = new JTextField(25);
 
+        sortPopup = new SortOptionJFrame(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "DUCK YOU!");
+            }
+        });
+
         add(searchButton);
         add(searchField);
         add(filterButton);
         add(sortButton);
 
+        // add some listeners
+        ActionListener searchListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                do_search();
+            }
+        };
+
+        searchField.addActionListener(  searchListener);
+        searchButton.addActionListener( searchListener);
+        sortButton.addActionListener(   new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sortPopup.showOption(searchButton);
+            }
+        });
     }
+
+    public void do_search(){
+        JOptionPane.showMessageDialog(null, "Searching,, apparently");
+    }
+
+    public void sort_result(String t){
+    }
+
 }
