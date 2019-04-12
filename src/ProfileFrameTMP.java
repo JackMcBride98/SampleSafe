bb8a694c7b218da3add9e1f1931514c29b3ac1b6
Jie He <Jiehe1998yd@gmail.com>
2019-04-12 15:45:11 +0100

    + Patched profile pic

--- src/ProfileFrameTMP.java
+++ src/ProfileFrameTMP.java
@@ -95,30 +95,33 @@ public class ProfileFrameTMP extends CustomPopUp {
         BufferedImage master = null;
         try {
             master = ImageIO.read(new File(System.getProperty("user.home") + "\\Desktop\\download.jpg"));
+
+            int diameter = Math.min(master.getWidth(), master.getHeight());
+            BufferedImage mask = new BufferedImage(master.getWidth(), master.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+            //source: https://stackoverflow.com/questions/31423130/how-to-make-circle-image-label-in-java
+
+            Graphics2D g2d = mask.createGraphics();
+            applyQualityRenderingHints(g2d);
+            g2d.fillOval(0, 0, diameter - 1, diameter - 1);
+            g2d.dispose();
+
+            BufferedImage masked = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
+            g2d = masked.createGraphics();
+            applyQualityRenderingHints(g2d);
+            int x = (diameter - master.getWidth()) / 2;
+            int y = (diameter - master.getHeight()) / 2;
+            g2d.drawImage(master, x, y, null);
+            g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.DST_IN));
+            g2d.drawImage(mask, 0, 0, null);
+            g2d.dispose();
+
+            return masked;
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return (new BufferedImage(128, 128, BufferedImage.TYPE_INT_ARGB));
 
-        int diameter = Math.min(master.getWidth(), master.getHeight());
-        BufferedImage mask = new BufferedImage(master.getWidth(), master.getHeight(), BufferedImage.TYPE_INT_ARGB);
-
-        //source: https://stackoverflow.com/questions/31423130/how-to-make-circle-image-label-in-java
-
-        Graphics2D g2d = mask.createGraphics();
-        applyQualityRenderingHints(g2d);
-        g2d.fillOval(0, 0, diameter - 1, diameter - 1);
-        g2d.dispose();
-
-        BufferedImage masked = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
-        g2d = masked.createGraphics();
-        applyQualityRenderingHints(g2d);
-        int x = (diameter - master.getWidth()) / 2;
-        int y = (diameter - master.getHeight()) / 2;
-        g2d.drawImage(master, x, y, null);
-        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.DST_IN));
-        g2d.drawImage(mask, 0, 0, null);
-        g2d.dispose();
-
-        return masked;
     }
 }
